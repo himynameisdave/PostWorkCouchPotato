18622983300672caea620b17a44a6c1730c49f75
himynameisdave <d@velunny.com>
2017-01-29 20:27:43 -0800

    Minor cleanup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,4 @@
 export * from './settings.js';
-export * from './videos.js';
 //  TODO: lets make videos just import them all?
 export * from './videosFetch.js';
 export * from './videosPlayer.js';
--- src/actions/videosPlayer.js
+++ src/actions/videosPlayer.js
@@ -1,20 +1,4 @@
 
-
-export const playerNextVideo = nextVideo => ({
-  type: 'PLAYER_GO_TO_NEXT_VIDEO',
-  payload: nextVideo
-});
-
-export const playerPrevVideo = prevVideo => ({
-  type: 'PLAYER_GO_TO_PREV_VIDEO',
-  payload: prevVideo
-});
-
-export const playerLoadVideos = () => ({
-  type: 'PLAYER_LOAD_VIDEOS'
-});
-
-// export const loadVideos = (activeVideo, nextVideo) => ({
-//   type: 'LOAD_VIDEOS',
-//   payload: { activeVideo, nextVideo }
-// });
+export const playerNextVideo = () => ({ type: 'PLAYER_GO_TO_NEXT_VIDEO', });
+export const playerPrevVideo = () => ({ type: 'PLAYER_GO_TO_PREV_VIDEO', });
+export const playerLoadVideos = () => ({ type: 'PLAYER_LOAD_VIDEOS' });
