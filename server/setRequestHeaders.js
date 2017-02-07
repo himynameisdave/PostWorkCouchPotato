08bf67e0902440087cfd0c42e0151ea94a28a9b5
himynameisdave <d@velunny.com>
2017-02-06 23:04:20 -0800

    Writes tests for server/setRequestHeaders.js

--- server/setRequestHeaders.js
+++ server/setRequestHeaders.js
@@ -1,10 +1,7 @@
-
 const setRequestHeaders = (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', false);
 
   next();
